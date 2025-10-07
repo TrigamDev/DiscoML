7310f4dbec251ae677f900e1e4bbc56097752ba6
TrigamDev <trigambusiness@gmail.com>
2025-10-07 14:50:42 -0400

    Reformat location.ts (and some tweaks)

--- src/lang/location.ts
+++ src/lang/location.ts
@@ -1,56 +1,100 @@
-import { lineBreak } from "@disco/constants";
+import { lineBreak } from "@disco/constants"
+
+const startingLine = 1
+const startingColumn = 1
+const startingOffset = 0
 
 class Location {
 	line: number
 	column: number
 	offset: number
-	constructor(line = 1, column = 1, offset = 0) {
-		this.line = line;
-		this.column = column;
-		this.offset = offset;
+	constructor (
+		line: number = startingLine,
+		column: number = startingColumn,
+		offset: number = startingOffset
+	) {
+		this.line = line
+		this.column = column
+		this.offset = offset
 	}
 
-	clone() {
-		return structuredClone(this);
+	clone (): Location {
+		return structuredClone( this )
 	}
 
-	toString() {
-		return `line ${this.line}, column ${this.column}`;
+	toString (): string {
+		return `Location(${ this.line }:${ this.column }, ${ this.offset })`
 	}
+
+	toJSON (): LocationObject {
+		return {
+			line: this.line,
+			column: this.column,
+			offset: this.offset
+		}
+	}
+}
+interface LocationObject {
+	line: number
+	column: number
+	offset: number
 }
 
 class LocationSpan {
 	start: Location
 	end: Location
-	constructor(start = new Location(), end = new Location()) {
-		this.start = start;
-		this.end = end;
+	constructor (
+		start: Location = new Location(),
+		end: Location = new Location()
+	) {
+		this.start = start
+		this.end = end
 	}
 
-	clone() {
-		return structuredClone(this);
+	clone (): LocationSpan {
+		return structuredClone( this )
 	}
 
-	snapToEnd() {
-		this.end = this.start.clone();
+	snapToEnd (): void {
+		this.end = this.start.clone()
 	}
 
-	forward(char: string) {
-		this.end.column++;
-		this.end.offset++;
+	forward ( char: string ): void {
+		this.end.column++
+		this.end.offset++
 
-		if (lineBreak.test(char)) {
-			this.end.column = 1;
-			this.end.line++;
+		if ( lineBreak.test( char ) ) {
+			this.end.column = 1
+			this.end.line++
 		}
 	}
 
-	between(other: LocationSpan): LocationSpan {
-		const start = this.start.offset > other.start.offset ? this.start : other.start;
-		const end = this.end.offset > other.end.offset ? this.end : other.end;
+	between ( other: LocationSpan ): LocationSpan {
+		const start = this.start.offset > other.start.offset ? this.start : other.start
+		const end = this.end.offset > other.end.offset ? this.end : other.end
 
-		return new LocationSpan(start.clone(), end.clone());
+		return new LocationSpan( start.clone(), end.clone() )
 	}
-};
 
-export { Location, LocationSpan };
+	toString (): string {
+		return "LocationSpan("
+			+ `start=(${ this.start.line }:${ this.start.column }, ${ this.start.offset }), `
+			+ `end=(${ this.end.line }:${ this.end.column }, ${ this.end.offset })`
+			+ ")"
+	}
+
+	toJSON (): LocationSpanObject {
+		return {
+			start: this.start.toJSON(),
+			end: this.end.toJSON()
+		}
+	}
+}
+interface LocationSpanObject {
+	start: LocationObject
+	end: LocationObject
+}
+
+export {
+	Location, LocationSpan
+}
